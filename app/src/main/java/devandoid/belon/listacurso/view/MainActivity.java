ed1cf517b86e3d5f9afbd59954ccd6cfbae716f7
Gustavo Belon <gubelon@outlook.com>
2023-06-11 17:33:09 -0300

    criando pessoas e testando com debug

--- app/src/main/java/devandoid/belon/listacurso/view/MainActivity.java
+++ app/src/main/java/devandoid/belon/listacurso/view/MainActivity.java
@@ -10,6 +10,7 @@ import devandoid.belon.listacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +18,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+        pessoa.setPrimeiroNome("Gustavo");
+        pessoa.setSobreNome("Belon");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("11-944903993");
+
+        outraPessoa= new Pessoa();
+        outraPessoa.setPrimeiroNome("Lucas");
+        outraPessoa.setSobreNome("Belon");
+        outraPessoa.setCursoDesejado("Android");
+        outraPessoa.setTelefoneContato("11-996008145");
+
     }
 }
